f661a35e97f09d3ac64d3614dcad3aa384afeca3
masayuki-y <yamada.masayuki.yk6@is.naist.jp>
2021-01-07 13:48:09 +0900

    dd_

---
+++ my_first_controller/src/MyFirstController.cpp
@@ -0,0 +1,53 @@
+#include "MyFirstController.h"
+
+MyFirstController::MyFirstController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
+: mc_control::MCController(rm, dt)
+{
+  jointIndex = robot().jointIndexByName(jointName);
+
+  config_.load(config);
+  solver().addConstraintSet(contactConstraint);
+  solver().addConstraintSet(kinematicsConstraint);
+  solver().addTask(postureTask);
+  solver().setContacts({{}});
+
+  mc_rtc::log::success("MyFirstController init done");
+}
+
+bool MyFirstController::run()
+{
+  if(std::abs(postureTask->posture()[jointIndex][0] - robot().mbc().q[jointIndex][0]) < 0.05)
+  {
+    switch_target();
+  }
+  return mc_control::MCController::run();
+}
+
+void MyFirstController::reset(const mc_control::ControllerResetData & reset_data)
+{
+  mc_control::MCController::reset(reset_data);
+}
+
+void MyFirstController::switch_target()
+{
+  if(goingLeft)
+  {
+    //postureTask->target({{jointName, robot().qu()[jointIndex]}});
+    postureTask->target({{jointName, robot().ql()[jointIndex]}});
+  }
+  else
+  {
+    postureTask->target({{jointName, robot().qu()[jointIndex]}});
+    //postureTask->target({{jointName, robot().ql()[jointIndex]}});
+  }
+  goingLeft = !goingLeft;
+  postureTask->target({{jointName2, robot().qu()[jointIndex]}});
+}
+
+/*void MyFirstController::switch_target2()
+{
+  postureTask->target({{jointName2, robot().qu()[jointIndex]}});
+  //goingLeft = !goingLeft;
+}*/
+
+CONTROLLER_CONSTRUCTOR("MyFirstController", MyFirstController)
---
+++ my_first_controller/src/MyFirstController.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <mc_control/mc_controller.h>
+
+#include "api.h"
+
+struct MyFirstController_DLLAPI MyFirstController : public mc_control::MCController
+{
+    MyFirstController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config);
+
+    bool run() override;
+
+    void reset(const mc_control::ControllerResetData & reset_data) override;
+
+    void switch_target();
+private:
+    mc_rtc::Configuration config_;
+    std::string jointName2 = "L_SHOULDER_P";
+    std::string jointName = "NECK_Y";
+    int jointIndex = 0;
+    bool goingLeft = true;
+    //bool goingLeft = false;
+};
