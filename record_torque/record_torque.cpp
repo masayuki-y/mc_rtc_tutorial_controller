bde0e467be37e49d9d160984adc1af1568eaae8d
masayuki-y <yamada.masayuki.yk6@is.naist.jp>
2021-11-22 03:33:12 +0900

    record torque as csv

--- record_torque/record_torque.cpp
+++ record_torque/record_torque.cpp
@@ -6,13 +6,13 @@ using std::endl;
 using std::ofstream;
 
 std::ofstream ofs_torque("/root/my_first_controller/src/jointTorque.csv");
-std::ofstream ofs_torque_part("/root/Record/Alljoints/jointTorque_part.csv");
 std::ofstream ofs_torque_three("/root/Record/joint/jointTorque_three.csv");
-std::ofstream ofs_torque_wrist("/root/Record/joint/jointTorque_wrist.csv");
-std::ofstream ofs_torque_elbow("/root/Record/joint/jjointTorque_elbow.csv");
-std::ofstream ofs_torque_sholder("/root//Record/joint/jointTorque_sholder.csv");
+std::ofstream ofs_torque_right_wrist("/root/Record/joint/");
+std::ofstream ofs_torque_right_elbow("/root/Record/joint/jointTorque__right_elbow.csv");
+std::ofstream ofs_torque_right_sholder("/root/Record/joint/jointTorque__right_sholder.csv");
 std::ofstream ofs_jointNumber("/root/my_first_controller/src/jointNumber.csv");
-std::ofstream ofs_torque_botharm("/root/my_first_controller/src/torque_botharm.csv");
+std::ofstream ofs_torque_part("/root/Record/Alljoints/jointTorque_All.csv");
+std::ofstream ofs_torque_botharm("/root/Record/Alljoints/jointTorque_botharm.csv");
 
 int t;
 
@@ -40,12 +40,18 @@ bool MyFirstController::run()
   //id18=R_SHOULDER_P -> id24=R_WRIST_Y
   for(unsigned int i; i<43; ++i) 
       //ofs_torque_part<< t <<", ";
-      ofs_torque_part<< i <<", "<<robots().robot(0).jointTorques()[0+i]<< ", ";
+      ofs_torque_part<<robots().robot(0).jointTorques()[0+i]<<", ";
       ofs_torque_part<< std::endl;
 
-  ofs_torque_sholder<< t <<", "<< robots().robot(0).jointTorques()[21]<< ", "<< robots().robot(0).jointTorques()[20] << ", "<< robots().robot(0).jointTorques()[22]<< std::endl;
-  ofs_torque_elbow<< t <<", "<< robots().robot(0).jointTorques()[23]<< ", "<< robots().robot(0).jointTorques()[24]<< std::endl;
-  ofs_torque_wrist<< t <<", "<< robots().robot(0).jointTorques()[25]<< ", "<< robots().robot(0).jointTorques()[26] << std::endl;
+for(unsigned int i; i<26; ++i) 
+      //ofs_torque_part<< t <<", ";
+      //id18=right sholder id43=left lefthand 
+      ofs_torque_botharm<<robots().robot(0).jointTorques()[18+i]<< ", ";
+      ofs_torque_botharm<< std::endl;
+
+  ofs_torque_right_sholder<< t <<", "<< robots().robot(0).jointTorques()[21]<< ", "<< robots().robot(0).jointTorques()[20] << ", "<< robots().robot(0).jointTorques()[22]<< std::endl;
+  ofs_torque_right_elbow<< t <<", "<< robots().robot(0).jointTorques()[23]<< ", "<< robots().robot(0).jointTorques()[24]<< std::endl;
+  ofs_torque_right_wrist<< t <<", "<< robots().robot(0).jointTorques()[25]<< ", "<< robots().robot(0).jointTorques()[26] << std::endl;
   t=t+1;
 
   return mc_control::MCController::run();
